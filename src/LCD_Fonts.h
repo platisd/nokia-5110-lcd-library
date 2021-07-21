4377d45f04ee6d5dd6816649a29a44d1236b8789
Jonathas Barbosa <jnthas@gmail.com>
2021-07-20 21:54:49 -0300

    Small changes

--- src/LCD_Fonts.h
+++ src/LCD_Fonts.h
@@ -15,37 +15,38 @@
 
 class LcdFont
 {
-    public:
-        LcdFont()
-            : kFontTable( Nokia_LCD_Fonts::kDefault_font ) 
-            , hSpace( Nokia_LCD_Fonts::hSpace )
-            , hSpaceSize( Nokia_LCD_Fonts::hSpaceSize )
-            , kCharacterOffset( 0x20 )
-            , columnSize( Nokia_LCD_Fonts::kColumns_per_character )
-            , rowSize( Nokia_LCD_Fonts::kRows_per_character )
-        {
-        }
-        LcdFont(const unsigned char *hSpace,
-                    uint8_t hSpaceSize,
-                    uint8_t characterOffset,
-                    uint8_t pColumnSize,
-                    uint8_t pRowSize)
-            : hSpace( hSpace )
-            , hSpaceSize( hSpaceSize )
-            , kCharacterOffset( characterOffset )
-            , columnSize( pColumnSize )
-            , rowSize( pRowSize ) 
-        {
-        }
-        const unsigned char* getFont(char character) const
-        {
-            return *(kFontTable + (character - kCharacterOffset));
-        }
-        const uint8_t columnSize;
-        const uint8_t rowSize;
-        const unsigned char* hSpace;
-        const uint8_t hSpaceSize;        
-    private:
-        const unsigned char (*kFontTable)[Nokia_LCD_Fonts::kColumns_per_character];  //<- not good, I need other sizes here not only 5
-        const uint8_t kCharacterOffset;
+public:
+    LcdFont()
+        : kFontTable( Nokia_LCD_Fonts::kDefault_font ) 
+        , hSpace( Nokia_LCD_Fonts::hSpace )
+        , hSpaceSize( Nokia_LCD_Fonts::hSpaceSize )
+        , kCharacterOffset( 0x20 )
+        , columnSize( Nokia_LCD_Fonts::kColumns_per_character )
+        , rowSize( Nokia_LCD_Fonts::kRows_per_character )
+    {
+    }
+    LcdFont(//const unsigned char **fontTable,
+            const unsigned char *hSpace,
+                uint8_t hSpaceSize,
+                uint8_t characterOffset,
+                uint8_t pColumnSize,
+                uint8_t pRowSize)
+        : hSpace( hSpace )
+        , hSpaceSize( hSpaceSize )
+        , kCharacterOffset( characterOffset )
+        , columnSize( pColumnSize )
+        , rowSize( pRowSize ) 
+    {
+    }
+    const unsigned char* getFont(char character) const
+    {
+        return *(kFontTable + (character - kCharacterOffset));
+    }
+    const uint8_t columnSize;
+    const uint8_t rowSize;
+    const unsigned char* hSpace;
+    const uint8_t hSpaceSize;        
+private:
+    const unsigned char (*kFontTable)[Nokia_LCD_Fonts::kColumns_per_character];  //<- not good, I need other sizes here not only 5
+    const uint8_t kCharacterOffset;
 };
